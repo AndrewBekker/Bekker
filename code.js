ba02b50ec1f54c6c1646c38968f6e98761521771
Andrew <andrewbekkerzt@gmail.com>
2022-03-19 14:32:50 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,43 @@
+'use strick';
+
+// alert('Andrew');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(typeof(answer));
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(answers);
+
+
+// const user = 'Andrew';
+
+// alert(`Hello, ${user}`);
+
+
+// let a = 'arr' + '- object';
+//  console.log('arr' + '- object');
+
+
+// let incr = 10,
+//     decr = 10;
+
+ 
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+
+
